4783a06408361eb64cc80fabe7261971bf4335c5
nuts-rice <msis44443@gmail.com>
2021-10-12 14:59:09 -0400

    wew yay staking

--- packages/hardhat/contracts/stakeContract.sol
+++ packages/hardhat/contracts/stakeContract.sol
@@ -3,31 +3,74 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
-import "./ExampleExternalContract";
+import "./ExampleExternalContract.sol";
 
 
-contract Staker {
-
-  //event SetPurpose(address sender, string purpose);
+contract Greeter{
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping(address => uint256) public balance;
+  uint256 public constant threshold = 1 ether;
+  bool public isActive = false;
+  uint256 public deadline = block.timestamp + 30 seconds;
 
-  //Balance of users funds
-  mapping(address => uint256) public balances;
+  event Stake (address indexed sender, uint256 amount);
 
-  //threshold for staking
-  uint256 public constant threashold = 1 ether;
+  //@notice modifier to contract that requires deadline to be reached or not
+  modifier deadlineReached (bool requireReached){
+    uint256 timeRemaining = timeLeft();
+    if (requireReached){
+      require (timeRemaining == 0, "deadline is not reached yet");
+    } else {
+      require (timeRemaining > 0, "Deadline not reached yet");
+    }
+    _;
+  }
 
-  event Stake(address indexed sender, uint256 amount)
+  //@notice modifier that requires external contract to not be completed
+  modifier stakeNotCompleted() {
+    bool completed = exampleExternalContract.completed();
+    require (!completed, "staking process already completed");
+    _;
+  }
 
-  // @param exampleExternalContractAddress is contract that will hold staked funds
-  constructor(address exampleExternalContractAddress) public{
+  constructor(address exampleExternalContractAddress) {
+    // what should we do on deploy?
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  function stake public payable {
-    balances[msg.sender] += msg.value;
-      emit Stake(msg.sender, msg.value);
+  function execute() public stakeNotCompleted deadlineReached(false){
+    uint256 contractBalance = address(this).balance;
+
+    require (contractBalance >= threshold, "threshold not reached");
+
+    (bool sent,) = address(exampleExternalContract).call{value: contractBalance}(abi.encodeWithSignature("complete()"));
+    require (sent, "exampleExternalContract.complete failed");
+  }
+
+  function stake() public payable deadlineReached(false) stakeNotCompleted {
+    balance[msg.sender]  += msg.value;
+
+    emit Stake(msg.sender, msg.value);
+  }
+
+  function withdraw() public deadlineReached(true) stakeNotCompleted{
+    uint256 userBalance = balance[msg.sender];
+
+    require(userBalance > 0, "you dont have the balance to withdraw");
+
+    balance[msg.sender] = 0;
+
+    (bool sent, ) = msg.sender.call{value: userBalance}("");
+    require (sent, "Failed to send user balance back to user");
+  }
+
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    } else {
+      return deadline -  block.timestamp;
+    }
   }
 }
